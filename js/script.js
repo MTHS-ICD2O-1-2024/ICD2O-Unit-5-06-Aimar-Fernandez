94a53417415f7ed20f9e966a1e4ec79d50f959af
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-02 17:27:16 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  //input
+  let counter = 0
+  let answer = 0
+  const firstValue = parseint(document.getElementById("first-value").value)
+  const secondValue = parseint(document.getElementById("second-value").value)
+  //process
+  while (counter < secondValue) {
+    let counter = counter + 1
+    let answer = answer + firstValue
+  }
+  document.getElementById("answer").innerHTML = answer
+}
